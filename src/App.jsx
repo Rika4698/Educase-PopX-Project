519334aac088dd3d3119b0c72e26854df31a505e
Rika4698 <rikasharmin181@gmail.com>
2025-05-18 01:28:30 +0600

    create home page with responsive design

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,17 @@
 
 import './App.css'
-
+import { Route, Routes } from "react-router";
+import Home from './Pages/Home';
 function App() {
  
 
   return (
-    <>
-      
-    </>
+    <div className='min-h-screen flex justify-center items-center gap-2'>
+      <Routes>
+        <Route path='/' element={<Home/>}></Route>
+
+      </Routes>
+    </div>
   )
 }
 
